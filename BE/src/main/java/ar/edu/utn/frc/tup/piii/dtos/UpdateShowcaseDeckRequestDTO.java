d3b0575544bff16585148a725a096513ec241a33
lucas-bima <421552@tecnicatura.frc.utn.edu.ar>
2026-05-28 15:37:24 -0300

    Module 4: Implement showcased deck feature and PUT /api/users/profile/showcase/deck endpoint in UserController

---
+++ BE/src/main/java/ar/edu/utn/frc/tup/piii/dtos/UpdateShowcaseDeckRequestDTO.java
@@ -0,0 +1,14 @@
+package ar.edu.utn.frc.tup.piii.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateShowcaseDeckRequestDTO {
+    private Long deckId;
+}
