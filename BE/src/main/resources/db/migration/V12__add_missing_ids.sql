e78785c8d136428835154c8590d3a944ce1e5383
Lantieri-Martin <421312@tecnicatura.frc.utn.edu.ar>
2026-06-05 14:43:43 -0300

    fix: resolve Flyway migration version conflict

--- BE/src/main/resources/db/migration/V12__add_missing_ids.sql
+++ BE/src/main/resources/db/migration/V12__add_missing_ids.sql
@@ -1,9 +1,5 @@
 -- V10__add_missing_ids.sql
 
--- Add missing id column for user_showcase
-ALTER TABLE user_showcase DROP CONSTRAINT user_showcase_pkey;
-ALTER TABLE user_showcase ADD COLUMN id BIGSERIAL PRIMARY KEY;
-ALTER TABLE user_showcase ADD CONSTRAINT user_showcase_user_slot_uk UNIQUE (user_id, slot_position);
 
 -- Add missing id column for user_mutes
 ALTER TABLE user_mutes DROP CONSTRAINT user_mutes_pkey;
