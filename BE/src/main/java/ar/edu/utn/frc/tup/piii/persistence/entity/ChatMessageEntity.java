570620140ceb9736fb06e3a1a3f35309f5fb6b63
Lucas-Rojo-421581-2w1 <421581@tecnicatura.frc.utn.edu.ar>
2026-06-06 13:32:47 -0300

    feat(db): add JPA entities and repositories for friendships and chat

---
+++ BE/src/main/java/ar/edu/utn/frc/tup/piii/persistence/entity/ChatMessageEntity.java
@@ -0,0 +1,42 @@
+package ar.edu.utn.frc.tup.piii.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "chat_messages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private UserEntity sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private UserEntity receiver;
+
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
