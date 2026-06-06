a3a6b42c95f453da3e8f58ed8b65def0f4307bca
Lucas-Rojo-421581-2w1 <421581@tecnicatura.frc.utn.edu.ar>
2026-06-06 13:54:12 -0300

    feat(services): add frontend services for friends REST API and STOMP WebSockets (steps 14-15)

---
+++ FE/src/app/core/services/friends-api.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FriendshipDTO, PublicProfileDTO, ChatMessageDTO } from '../models/friends.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FriendsApiService {
+  private http = inject(HttpClient);
+  private readonly API_URL = 'http://localhost:8081/api';
+
+  sendFriendRequest(targetUsername: string): Observable<any> {
+    return this.http.post(`${this.API_URL}/friends/request`, { targetUsername });
+  }
+
+  getActiveFriends(): Observable<FriendshipDTO[]> {
+    return this.http.get<FriendshipDTO[]>(`${this.API_URL}/friends/list`);
+  }
+
+  getPendingRequests(): Observable<FriendshipDTO[]> {
+    return this.http.get<FriendshipDTO[]>(`${this.API_URL}/friends/requests`);
+  }
+
+  acceptFriendRequest(id: number): Observable<any> {
+    return this.http.put(`${this.API_URL}/friends/accept/${id}`, {});
+  }
+
+  rejectFriendRequest(id: number): Observable<any> {
+    return this.http.put(`${this.API_URL}/friends/reject/${id}`, {});
+  }
+
+  removeFriend(id: number): Observable<any> {
+    return this.http.delete(`${this.API_URL}/friends/remove/${id}`);
+  }
+
+  getPublicProfile(username: string): Observable<PublicProfileDTO> {
+    return this.http.get<PublicProfileDTO>(`${this.API_URL}/users/${username}/profile/public`);
+  }
+
+  getChatHistory(username: string): Observable<ChatMessageDTO[]> {
+    return this.http.get<ChatMessageDTO[]>(`${this.API_URL}/friends/chat/${username}`);
+  }
+}
