5a9de62bb46ca210ded7971537b98a5d16a2729f
Lucas-Rojo-421581-2w1 <421581@tecnicatura.frc.utn.edu.ar>
2026-06-06 14:06:27 -0300

    feat(ui): add ChatModalComponent for 1v1 messaging and challenges

---
+++ FE/src/app/shared/components/chat-modal/chat-modal.component.ts
@@ -0,0 +1,79 @@
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FriendshipDTO, ChatMessageDTO } from '../../../core/models/friends.models';
+import { FriendsApiService } from '../../../core/services/friends-api.service';
+import { FriendsWsService } from '../../../core/services/friends-ws.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-chat-modal',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './chat-modal.component.html',
+  styleUrls: ['./chat-modal.component.css']
+})
+export class ChatModalComponent implements OnInit, OnDestroy {
+  @Input() friend!: FriendshipDTO;
+  @Output() close = new EventEmitter<void>();
+
+  messages: ChatMessageDTO[] = [];
+  newMessage = '';
+  private sub?: Subscription;
+
+  constructor(
+    private friendsApi: FriendsApiService,
+    private friendsWs: FriendsWsService
+  ) {}
+
+  ngOnInit() {
+    this.friendsApi.getChatHistory(this.friend.friendUsername).subscribe(history => {
+      this.messages = history;
+      this.scrollToBottom();
+    });
+
+    this.sub = this.friendsWs.messages$.subscribe(msg => {
+      if (msg.senderUsername === this.friend.friendUsername || msg.receiverUsername === this.friend.friendUsername) {
+        this.messages.push(msg);
+        this.scrollToBottom();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub?.unsubscribe();
+  }
+
+  sendMessage() {
+    if (!this.newMessage.trim()) return;
+    
+    this.friendsWs.sendChatMessage({
+      senderUsername: '',
+      receiverUsername: this.friend.friendUsername,
+      content: this.newMessage
+    });
+    this.newMessage = '';
+  }
+
+  sendChallenge() {
+    const lobbyId = 'lobby_' + Date.now();
+    this.friendsWs.sendChallenge({
+      senderUsername: '',
+      receiverUsername: this.friend.friendUsername,
+      lobbyId
+    });
+  }
+
+  onClose() {
+    this.close.emit();
+  }
+
+  private scrollToBottom() {
+    setTimeout(() => {
+      const container = document.querySelector('.chat-messages');
+      if (container) {
+        container.scrollTop = container.scrollHeight;
+      }
+    }, 100);
+  }
+}
