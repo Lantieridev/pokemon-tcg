6a9f279ffb352c7b19e4268ee7eac2ed27f0a534
Lucas-Rojo-421581-2w1 <421581@tecnicatura.frc.utn.edu.ar>
2026-06-06 13:37:05 -0300

    feat(services): implement send friend request and listing logic

---
+++ BE/src/main/java/ar/edu/utn/frc/tup/piii/services/FriendshipService.java
@@ -0,0 +1,11 @@
+package ar.edu.utn.frc.tup.piii.services;
+
+import ar.edu.utn.frc.tup.piii.dtos.friends.FriendshipDTO;
+
+import java.util.List;
+
+public interface FriendshipService {
+    void sendFriendRequest(String senderUsername, String targetUsername);
+    List<FriendshipDTO> getActiveFriends(String username);
+    List<FriendshipDTO> getPendingRequests(String username);
+}
