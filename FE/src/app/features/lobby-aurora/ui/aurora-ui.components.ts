af4acba586870eb86a51cb84f7cdbc7f5d1f5874
lucas-bima <421552@tecnicatura.frc.utn.edu.ar>
2026-06-13 04:06:52 -0300

    feat(tutorial): implement lobby tutorial logic and help agent component

--- FE/src/app/features/lobby-aurora/ui/aurora-ui.components.ts
+++ FE/src/app/features/lobby-aurora/ui/aurora-ui.components.ts
@@ -17,6 +17,7 @@ import { RouterModule } from '@angular/router';
         @case ('sword') { <path d="M14 3h7v7M21 3l-9 9M3 21l5-1 4-4-4-4-4 4-1 5Z" /> }
         @case ('arrow') { <path d="M5 12h14M13 6l6 6-6 6" /> }
         @case ('fire') { <path d="M12 2c1 3 2.5 3.5 3.5 4.5A5 5 0 0 1 17 10a5 5 0 1 1-10 0c0-1.5.5-2.5 1.5-3.5C9.5 5.5 11 5 12 2z" /><path d="M12 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" /> }
+        @case ('help') { <circle cx="12" cy="12" r="10" /><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" /><line x1="12" y1="17" x2="12.01" y2="17" /> }
       }
     </svg>
   `
